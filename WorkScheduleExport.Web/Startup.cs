334ed6d183714df3223f7b965e5156d3e851b767
Johnny Hansson <johnny.hansson@tretton37.com>
2017-10-29 06:51:16 +0100

    Upgraded NuGet packages, added dockerfile for Raspberry Pi

--- WorkScheduleExport.Web/Startup.cs
+++ WorkScheduleExport.Web/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -47,6 +48,11 @@ namespace WorkScheduleExport.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
