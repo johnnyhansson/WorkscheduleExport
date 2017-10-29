706ec30509f8be338948131cd7cdf8abf78cf656
Johnny Hansson <johnny.hansson@tretton37.com>
2017-10-29 11:36:35 +0100

    Added missing dollar sign

--- tools/start-container.sh
+++ tools/start-container.sh
@@ -2,5 +2,5 @@ if [ ! "$(docker ps -q -f name=workscheduleexport)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=workscheduleexport)" ]; then
         docker rm -f workscheduleexport
     fi
-    docker run -d -p 5000:5000 --name workscheduleexport --restart=always -e BUILD=$BUILD_ID -e ASPNETCORE_URLS=$HOST_ADDRESS $DOCKER_REGISTRY_URL/workscheduleexport:BUILD_ID
+    docker run -d -p 5000:5000 --name workscheduleexport --restart=always -e BUILD=$BUILD_ID -e ASPNETCORE_URLS=$HOST_ADDRESS $DOCKER_REGISTRY_URL/workscheduleexport:$BUILD_ID
 fi
