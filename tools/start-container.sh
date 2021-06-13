b26117c10caed2a76effbde8b5c1498c977484dd
Johnny Cederholm <johnny.cederholm@tretton37.com>
2021-06-13 21:05:57 +0200

    Remove pipeline definition for Jenkins

--- tools/start-container.sh
+++
@@ -1,6 +0,0 @@
-if [ ! "$(docker ps -q -f name=workscheduleexport)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=workscheduleexport)" ]; then
-        docker rm -f workscheduleexport
-    fi
-    docker run -d -p 5000:5000 --name workscheduleexport --restart=always -e BUILD=$BUILD_ID -e ASPNETCORE_URLS=$HOST_ADDRESS $DOCKER_REGISTRY_ADDRESS/workscheduleexport:$BUILD_ID
-fi
